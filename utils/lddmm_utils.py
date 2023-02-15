afed81bcbff8d16e19d7c68c2e0d67611bbb54e8
tbesnier <mr.thomasbesnier@gmail.com>
2023-02-15 17:53:29 +0100

    tests on mouth curves

--- utils/lddmm_utils.py
+++ utils/lddmm_utils.py
@@ -144,7 +144,7 @@ def lossVarifoldSurf(FS, VT, FT, K):
 
     return loss
 
-def normVarifoldSurf(V,F, K):
+def NormVarifoldSurf(V,F, K):
     def get_center_length_normal(F, V):
         V0, V1, V2 = (
             V.index_select(0, F[:, 0]),
@@ -158,3 +158,131 @@ def normVarifoldSurf(V,F, K):
     C, L, Nn = get_center_length_normal(F, V)
     cst = (L * K(C, C, Nn, Nn, L)).sum()
     return(cst)
+
+def DotVarifoldCurv(VT, K, normalized=False):
+    """Compute varifold distance between two meshes
+
+    Input: 
+        - FS: face connectivity of source mesh
+        - VT: vertices of target mesh [nVx3 torch tensor]
+        - FT: face connectivity of target mesh [nFx3 torch tensor]
+        - K: kernel
+    Output:
+        - loss: function taking VS (vertices coordinates of source mesh)
+    """
+    def get_center_length_tangent(curve, device='cuda:0'):
+    
+        id_start = torch.arange(0, curve.shape[0]-1).to(dtype=torch.long, device=device)
+        id_end = torch.arange(1, curve.shape[0]).to(dtype=torch.long, device=device)
+
+        V_start, V_end = curve[id_start], curve[id_end]
+
+        centers = (V_start + V_end)/2
+        lengths = ((V_end - V_start)**2).sum(dim=1)[:, None].clamp_(min=1e-10).sqrt()
+        tangents = (V_end - V_start)/(lengths)
+
+        return(centers, lengths, tangents)
+
+    CT, LT, NTn = get_center_length_tangent(VT)
+    #cst = (LT * K(CT, CT, NTn, NTn, LT)).sum()
+    def loss(VS):
+        CS, LS, NSn = get_center_length_tangent(VS)
+        if normalized:
+            return ((LS * K(CS, CT, NSn, NTn, LT)).sum()) / (NormVarifoldCurv(VT, K)*NormVarifoldCurv(VS,K) )
+        else:
+            return (LS * K(CS, CT, NSn, NTn, LT)).sum()
+    return loss
+
+def NormVarifoldCurv(VT, K):
+    def get_center_length_tangent(curve, device='cuda:0'):
+    
+        id_start = torch.arange(0, curve.shape[0]-1).to(dtype=torch.long, device=device)
+        id_end = torch.arange(1, curve.shape[0]).to(dtype=torch.long, device=device)
+
+        V_start, V_end = curve[id_start], curve[id_end]
+
+        centers = (V_start + V_end)/2
+        lengths = ((V_end - V_start)**2).sum(dim=1)[:, None].clamp_(min=1e-10).sqrt()
+        tangents = (V_end - V_start)/(lengths)
+
+        return(centers, lengths, tangents)
+
+    CT, LT, NTn = get_center_length_tangent(VT)
+    cst = (LT * K(CT, CT, NTn, NTn, LT)).sum()
+
+    return cst
+
+def lossVarifoldCurv(VT, K, normalized=False):
+    """Compute varifold distance between two meshes
+
+    Input: 
+        - FS: face connectivity of source mesh
+        - VT: vertices of target mesh [nVx3 torch tensor]
+        - FT: face connectivity of target mesh [nFx3 torch tensor]
+        - K: kernel
+    Output:
+        - loss: function taking VS (vertices coordinates of source mesh)
+    """
+    def get_center_length_tangent(curve, device='cuda:0'):
+    
+        id_start = torch.arange(0, curve.shape[0]-1).to(dtype=torch.long, device=device)
+        id_end = torch.arange(1, curve.shape[0]).to(dtype=torch.long, device=device)
+
+        V_start, V_end = curve[id_start], curve[id_end]
+
+        centers = (V_start + V_end)/2
+        lengths = ((V_end - V_start)**2).sum(dim=1)[:, None].clamp_(min=1e-12).sqrt()
+        tangents = (V_end - V_start)/(lengths)
+
+        return(centers, lengths, tangents)
+
+    CT, LT, NTn = get_center_length_tangent(VT)
+    if normalized:
+        cst = (LT * K(CT, CT, NTn, NTn, LT)).sum() / NormVarifoldCurv(VT, K)**2
+    else:
+        cst = (LT * K(CT, CT, NTn, NTn, LT)).sum()
+    def loss(VS):
+        CS, LS, NSn = get_center_length_tangent(VS)
+        if normalized:
+            return (
+                cst
+                + (LS * K(CS, CS, NSn, NSn, LS)).sum()/NormVarifoldCurv(VS,K)**2
+                - 2 * (LS * K(CS, CT, NSn, NTn, LT)).sum()/NormVarifoldCurv(VS,K)*NormVarifoldCurv(VT,K)
+            )
+        else:
+            return (
+                cst
+                + (LS * K(CS, CS, NSn, NSn, LS)).sum()
+                - 2 * (LS * K(CS, CT, NSn, NTn, LT)).sum()
+            )
+
+    return loss
+
+def lossVarifoldCurv_ID(V, VS, FS, VT, FT, K):
+
+    def get_center_length_tangent(curve, device='cuda:0'):
+    
+        id_start = torch.arange(0, curve.shape[0]-1).to(dtype=torch.long, device=device)
+        id_end = torch.arange(1, curve.shape[0]).to(dtype=torch.long, device=device)
+
+        V_start, V_end = curve[id_start], curve[id_end]
+
+        centers = (V_start + V_end)/2
+        lengths = ((V_end - V_start)**2).sum(dim=1)[:, None].clamp_(min=1e-12).sqrt()
+        tangents = (V_end - V_start)/(lengths)
+
+        return(centers, lengths, tangents)
+
+    CT, LT, NTn = get_center_length_tangent(V)
+
+    cst = (LT * K(CT, CT, NTn, NTn, LT)).sum()/NormVarifoldSurf(VT,FT,K)**2
+    def loss(V2):
+        CS, LS, NSn = get_center_length_tangent(V2)
+
+        return (
+            cst
+            + (LS * K(CS, CS, NSn, NSn, LS)).sum()/NormVarifoldSurf(VS,FS,K)**2
+            - 2 * (LS * K(CS, CT, NSn, NTn, LT)).sum()/NormVarifoldSurf(VT,FT,K)*NormVarifoldSurf(VS,FS,K)
+        )
+
+    return loss
